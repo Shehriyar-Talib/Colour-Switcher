9c2988175bec69bffa8320b717261514c3febd71
Shehriyar-Talib <105674085+Shehriyar-Talib@users.noreply.github.com>
2022-12-29 18:13:39 +0500

    first message

---
+++ script.js
@@ -0,0 +1,23 @@
+var red = document.getElementById("red");
+var aqua = document.getElementById("aqua");
+var hotpink = document.getElementById("violent");
+var tomato = document.getElementById("tomato");
+
+red.addEventListener("click" , function(){
+    document.body.style.backgroundColor  = "#ff0000"
+    
+
+    
+})
+aqua.addEventListener("click" , function(){
+    document.body.style.backgroundColor  = "#7fffd4"
+    
+})
+hotpink.addEventListener("click" , function(){
+    document.body.style.backgroundColor  = "#FF69B4"
+    
+})
+tomato.addEventListener("click" , function(){
+    document.body.style.backgroundColor  = "#FF6347"
+    
+})
